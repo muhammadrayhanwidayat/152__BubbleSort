128d57b4e13db72e01e7a5bfddc5f9e026fbee95
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-07 13:51:55 +0700

    menambahkan prosedur input

--- pertemuan3Algoritma.cpp
+++ pertemuan3Algoritma.cpp
@@ -4,6 +4,19 @@ using namespace std;
 int a[20];	//Deklarasi array a dengan ukuran 20
 int n;		//Deklarasi variable n untuk menyimpan banyaknya elemen pada array
 
+void input() {		//prosedur untuk input
+	while (true) {	//looping
+		cout << "masukan banyaknya elemen pada array:"; // output ke layar
+		cin >> n;			//input dari pengguna
+		if (n <= 20)		//jika n kurang dari atau samadengan 20
+			break;			//keluar dari lopp
+		else {				//jika n lebih dari 20
+			cout << "\Array dapat mempunyai maksimal 20 elemen.\n";//output ke layar
+
+		}
+	}
+
+}
 
 int main() {
 
